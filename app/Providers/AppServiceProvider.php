d92afa73e1162b6821287c2deee2090fa43ebcd0
Alexassassin <657449391@qq.com>
2018-07-27 08:27:52 +0000

    Finish user edit,update,index,and destroy actions

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,11 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    protected $policies = [
+        'App\Model' => 'App\Policies\ModelPolicy',
+        \App\Models\User::class => \App\Policies\UserPolicy::class,
+    ];
+
     /**
      * Bootstrap any application services.
      *
