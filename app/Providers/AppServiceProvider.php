4dc1633964431dbb382c44002fd4e93f120696c1
Alexassassin <657449391@qq.com>
2018-07-30 03:04:14 +0000

    Add user statuses

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Carbon\Carbon;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,7 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Carbon::setlocale('zh');
     }
 
     /**
